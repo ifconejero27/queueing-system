1fc09f2516934d177a25199bb6aa7f4c97233ecb
ifconejero27 <ifconejero27@gmail.com>
2026-09-07 17:54:07 +0800

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -156,6 +156,6 @@ app.post("/api/queue/complete", (req, res) => {
   res.json(currentStudent);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
